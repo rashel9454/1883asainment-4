bb496299a137d028c113d9f264ef09c2f90d0cc5
rashel9454 <rashelrana931@gmail.com>
2022-05-06 09:24:57 +0600

    practic class 4

---
+++ practic.php
@@ -0,0 +1,79 @@
+<?php
+// for($i=3; $i<=10; $i++){
+//     echo "ami parbo";
+// //     echo "<br>";
+// // }
+// // 
+
+// $name=20 ;
+// while($name>=1){
+//     echo "ami nay";
+//     echo "<br>";
+//     $name--;
+// }
+
+// $name=1;
+// do{
+//     echo "ami nay";
+//     echo "<br>";
+//     $name++ ;
+// }
+
+// for($i=2010; $i >=1990;  $i --){
+//     echo $i;
+//     echo "<br>";
+   
+// }
+// while($name<=10);
+
+// $i=2010;
+// while( $i >=1990 ){
+//     echo $i;
+//     echo "<br>";
+//     $i --;
+// }
+
+// $i=2010;
+// do{
+//     echo $i;
+//     echo "<br>";
+//     $i --;
+   
+// }while( $i >=1990);
+
+// $bortoman_boyosh= date("Y");
+// $initial_year=$bortoman_boyosh-20;
+// for($initial_year; $initial_year <= $bortoman_boyosh; $initial_year++){
+//     echo $initial_year;
+//     echo "<br>";
+// }
+// $bortoman_boyosh= date("Y");
+// $initial_year=$bortoman_boyosh-30;
+// while($initial_year<= $bortoman_boyosh){
+//         echo $initial_year;
+//         echo "<br>";
+//         $initial_year++;
+// }
+// $bortoman_boyosh= date("Y");
+// $initial_year=$bortoman_boyosh-40;
+// do {
+//         echo $initial_year;
+//         echo "<br>";
+//         $initial_year++;
+// }while($initial_year<= $bortoman_boyosh);
+
+// $number=[10, 20, 30, 40, 50];
+// var_dump($number);
+// array(5){ 
+//     [0]=> int(10) 
+//     [1]=> int(20) 
+//     [2]=> int(30) 
+//     [3]=> int(40) 
+//     [4]=> int(50) 
+// }
+// $number=['rashel', 'foysal', 30, 40, 50];
+// foreach($number as $key => $value ){
+//     echo $key. ' = ' . $value;
+//     echo "<br>";
+// }
+
